64c5ca6725c2a8dca7adaa2d541c65a50932a229
grzesiekzajac <grzesiek.zajac1@gmail.com>
2016-04-01 16:37:57 +0200

    RandomNetworks works with external voting systems

--- ziwm/examples/example_random_networks.py
+++ ziwm/examples/example_random_networks.py
@@ -2,10 +2,17 @@ from ziwm.data.iris.iris import IrisDataset
 from ziwm.data.utils import split_dataset
 from ziwm.model.ensemble.randomnetworks.random_networks import RandomNetworks
 from ziwm.model.utils import round_result
+from ziwm.model.base_classifier.extreme_learning_machine.extreme_learning_machine import ExtremeLearningMachine
+from ziwm.model.voting_system.arithmetic_mean.arithmetic_mean import ArithmeticMean
+from ziwm.model.voting_system.majority_voting.majority_voting import MajorityVoting
 
-model = RandomNetworks(2)
+elm = []
+for i in range(3):
+    elm.append(ExtremeLearningMachine())
+am = ArithmeticMean()
+mv = MajorityVoting()
+model = RandomNetworks(elm, mv)
 dataset = IrisDataset()
-
 X, Y = dataset.load()
 X_train, X_test, Y_train, Y_test = split_dataset(X, Y)
 
--- ziwm/model/ensemble/randomnetworks/random_networks.py
+++ ziwm/model/ensemble/randomnetworks/random_networks.py
@@ -1,24 +1,14 @@
 import numpy as np
-
-from ziwm.model.base_classifier.baseclassifier import BaseClassifier
-from ziwm.model.base_classifier.extreme_learning_machine.extreme_learning_machine import ExtremeLearningMachine
+from ziwm.model.ensemble.ensemble import Ensemble
 
 
-class RandomNetworks(BaseClassifier):
+class RandomNetworks(Ensemble):
     '''
     Description...
     '''
 
-    def __init__(self, committee_size=3):
-        '''
-        Description...
-        '''
-        self.__committee = []
-        if committee_size < 2:
-            committee_size = 2
-        for member in range(committee_size):
-            member = ExtremeLearningMachine()
-            self.__committee.append(member)
+    def __init__(self, base_classifiers, voting_system):
+        super(RandomNetworks, self).__init__(base_classifiers, voting_system)
 
     @staticmethod
     def name():
@@ -29,30 +19,20 @@ class RandomNetworks(BaseClassifier):
         Description...
         '''
         results = []
-        for member in self.__committee:
+        for member in self.base_classifiers:
             result = member.predict(x_test)
             results.append(result)
-        return self.__majority_voting(results)
+        print 'partial results:\n', results
+        return self.voting_system.vote(results)
 
     def train(self, x, y):
         '''
         Mozna przyspieszyc wykonujac search_param jednokrotnie
         i trenujac bezposrednio metoda member.extreme_learning_machine.train
         '''
-        for member in self.__committee:
+        for member in self.base_classifiers:
             member.train(x, y)
 
-
-    @staticmethod
-    def __majority_voting(results):
-        results_T = map(list, zip(*results))
-        print 'rezulataty transponowane: ', results_T
-        voting_result = []
-        for row in results_T:
-            rounded_row = [abs(int(round(n, 0))) for n in row]
-            counts = np.bincount(rounded_row)
-            voting_result.append(np.argmax(counts))
-        print 'wyniki glosowania: ', voting_result
 
 
 
