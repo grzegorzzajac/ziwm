b90624c58de4361a57b986129128d8f873f1cb76
Kuba Wężykowski <kuba.wezykowski@gmail.com>
2016-04-04 23:03:41 +0200

    Added random feature subspace ensemble

---
+++ ziwm/examples/model/example_random_subspace.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python2.7
+
+from ziwm.data.iris.iris import IrisDataset
+from ziwm.data.utils import split_dataset
+from ziwm.model.ensemble.random_subspace.random_subspace import RandomSubspace
+from ziwm.model.base_classifier.extreme_learning_machine.extreme_learning_machine import ExtremeLearningMachine
+from ziwm.model.voting_system.arithmetic_mean.arithmetic_mean import ArithmeticMean
+from ziwm.model.voting_system.majority_voting.majority_voting import MajorityVoting
+
+
+elm = []
+for i in range(3):
+    elm.append(ExtremeLearningMachine())
+am = ArithmeticMean()
+mv = MajorityVoting()
+model = RandomSubspace(elm, am)
+dataset = IrisDataset()
+X, Y = dataset.load()
+X_train, X_test, Y_train, Y_test = split_dataset(X, Y)
+
+model.train(X_train, Y_train)
+prediction = model.predict(X_test)
+
+print 'expected:\n', Y_test
+print 'predicted:\n', prediction
+
+#score = model_score(model, X_test, Y_test, problem_type='classification')
+#print score
---
+++ ziwm/model/ensemble/random_subspace/random_subspace.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python2.7
+
+import random
+import numpy as np
+from ziwm.model.ensemble.ensemble import Ensemble
+
+
+class RandomSubspace(Ensemble):
+
+    def __init__(self, base_classifiers, voting_system):
+        super(RandomSubspace, self).__init__(base_classifiers, voting_system)
+        self.feature_subspaces = []
+
+    @staticmethod
+    def name():
+        return "Random Subspace"
+
+    def predict(self, x_test):
+        results = []
+        assert len(self.feature_subspaces) == len(self.base_classifiers)
+        for i in range(len(self.base_classifiers)):
+            x_individual = self.__create_x_from_feature_subspace(x_test, self.feature_subspaces[i])
+            result = self.base_classifiers[i].predict(x_individual)
+            results.append(result)
+        return self.voting_system.vote(results)
+
+    def train(self, x, y):
+        self.feature_subspaces = []
+        for i in range(len(self.base_classifiers)):
+            new_x, feature_subspace = self.__create_individual_dataset(x)
+            self.base_classifiers[i].train(new_x, y)
+            self.feature_subspaces.append(feature_subspace)
+
+    @staticmethod
+    def __create_individual_dataset(x):
+        features_size = x.shape[1]
+        r = features_size / 2  # number of randomly picked features for subspace
+        feature_subspace = random.sample(xrange(features_size), r)
+        new_x = np.zeros((x.shape[0], r))
+        for i in range(r):
+            feature = feature_subspace[i]
+            new_x[:, i] = x[:, feature]
+        return new_x, feature_subspace
+
+    @staticmethod
+    def __create_x_from_feature_subspace(x, feature_subspace):
+        new_x = np.zeros((x.shape[0], len(feature_subspace)))
+        for i in range(len(feature_subspace)):
+            new_x[:, i] = x[:, feature_subspace[i]]
+        return new_x
