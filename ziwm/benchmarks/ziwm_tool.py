3d1b6631f0dfd39378edf176fcf39088b178ab8a
Sebastian Bujwid <sebastian@bujwid.eu>
2016-04-07 20:57:42 +0200

    Print dataset information

--- ziwm/benchmarks/ziwm_tool.py
+++ ziwm/benchmarks/ziwm_tool.py
@@ -5,6 +5,8 @@ from ziwm.data.base import Dataset
 from ziwm.data.utils import split_dataset
 from ziwm.benchmarks.validator import model_score
 
+import numpy as np
+
 if __name__ == '__main__':
     '''
     Tests performance of prediction models on all available datasets
@@ -15,12 +17,16 @@ if __name__ == '__main__':
     models = Classifier.all_models()
     
     # print output header
-    print("dataset,model,score")
+    print("dataset,dataset_size,number_of_classes,model,score")
 
     for dataset in datasets:
 
         # load mock_classifier
         X, Y = dataset.load()
+        # dataset info
+        dataset_size = X.shape[0]
+        classes_count = np.unique(Y).size
+
         
         # split mock_classifier into train and examples sets
         X_train, X_test, Y_train, Y_test = split_dataset(X, Y)
@@ -35,5 +41,6 @@ if __name__ == '__main__':
             score = model_score(model, X_test, Y_test, dataset.problem_type())
             
             # print results in csv format
-            print("{0},{1},{2}".format(dataset.name(), model.name(), score))
+            print("{0},{1},{2},{3},{4}".format(dataset.name(), dataset_size\
+                                               ,classes_count, model.name(), score))
             
