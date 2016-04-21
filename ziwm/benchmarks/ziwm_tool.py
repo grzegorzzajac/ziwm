efb064898fd55f186d0c29caf668172dc42ff02f
Sebastian Bujwid <sebastian@bujwid.eu>
2016-04-21 13:26:43 +0200

    Print feature_count info

--- ziwm/benchmarks/ziwm_tool.py
+++ ziwm/benchmarks/ziwm_tool.py
@@ -22,11 +22,11 @@ if __name__ == '__main__':
     voting_systems = VotingSystem.all_voting_systems()
     ensemble_types = Ensemble.all_ensemble_types()
     
-    output_string_format = "{0: <15}\t{1: <28}\t{2: <13}\t{3: <18}\t{4: <28}\t{5: <22}\t{6: <15}\t{7: <22}"
+    output_string_format = "{0: <15}\t{1: <28}\t{8: <15}{2: <13}\t{3: <18}\t{4: <28}\t{5: <22}\t{6: <15}\t{7: <22}"
 
     # print output header
     print(output_string_format
-          .format('score','dataset','dataset_size','number_of_classes','model','ensemble','ensemble_size','voting_system'))
+          .format('score','dataset','dataset_size','number_of_classes','model','ensemble','ensemble_size','voting_system', 'feature_count'))
 
     for dataset in datasets:
 
@@ -53,7 +53,7 @@ if __name__ == '__main__':
             print(output_string_format
                   .format(score, dataset.name(), dataset_size\
                           ,classes_count, model.name()\
-                          ,'NONE', 'NONE', 'NONE'))
+                          ,'NONE', 'NONE', 'NONE', X_train.shape[1]))
 
             # score enembles based on current model
             for voting_system in voting_systems:
@@ -61,7 +61,7 @@ if __name__ == '__main__':
                 for ensemble_type in ensemble_types:
                     
                     # create ensemble
-                    classifiers_in_ensamble = 5
+                    classifiers_in_ensamble = 50
                     ensemble = ensemble_type(voting_system, type(model), classifiers_in_ensamble)
                     ensemble.train(X_train, Y_train)
                     
@@ -73,4 +73,4 @@ if __name__ == '__main__':
                           .format(score, dataset.name(), dataset_size\
                                   ,classes_count, model.name()\
                                   ,ensemble.name(), classifiers_in_ensamble\
-                                  ,voting_system.name()))
+                                  ,voting_system.name(), X_train.shape[1]))
