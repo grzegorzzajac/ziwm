85e1bf17c06aa4bf2855196d04d31b80e7bf7ee5
grzesiekzajac <grzesiek.zajac1@gmail.com>
2016-05-29 14:03:58 +0200

    Add ploting type 1 - score/size for each dataset separately

--- ziwm/ploting/source/old/ploting_one_classifier_for_all_datasets.R
+++ ziwm/ploting/source/old/ploting_one_classifier_for_all_datasets.R
@@ -1,13 +1,17 @@
 library(ggplot2)
 
-dataset_name <- "elm_final_results"
+script.dir <- dirname(sys.frame(1)$ofile)
+
+dataset_name <- "svm_bagging_results"
+#dataset_name <- "elm_final_results"
 
 csv_name <- paste(dataset_name, ".csv", sep="")
-#csv_path <- file.path("..", "csv", csv_name)
+csv_path <- file.path(script.dir, "..", "csv", csv_name)
 data <- read.table(csv_name, sep =",", header = TRUE)
 
 ensembles = data
-#ensembles = subset(ensembles, dataset == "house_votes_84")
+ensembles = subset(ensembles, dataset != "fertility_yesnoreal")
+ensembles = subset(ensembles, dataset != "fertility_yesnoOHE")
 #ensembles = subset(ensembles, model != "MockClassifier")
 
 ensembles$ensemble_all = paste(ensembles$model, ensembles$ensemble, sep="_")
@@ -17,7 +21,6 @@ ggplot(ensembles, aes(ensemble_size, score, colour=ensemble_all)) +
   geom_point() + geom_line() + facet_wrap(~dataset) # + geom_smooth(aes(group=ensemble_all), se = FALSE)
 
 png_name <- paste(dataset_name, ".png", sep="")
-#png_path <- file.path(".", "png", png_name)
-#ggsave(png_path, width = 40, height = 20, units = "cm")
-ggsave(png_name, width = 100, height = 50, units = "cm")
+png_path <- file.path(script.dir, "..", "png", png_name)
+ggsave(png_path, width = 40, height = 20, units = "cm")
 
---
+++ ziwm/ploting/source/old/test_read_multiple_csv.R
@@ -0,0 +1,27 @@
+# ploting
+# score / ensemble size
+# each dataset separately
+# (uses all *.csv files in 'results' dir)
+
+script.dir <- dirname(sys.frame(1)$ofile)
+csv_path <- file.path(script.dir, "..", "..", "results")
+
+file_names <- dir(csv_path)
+print(file_names)
+
+setwd(csv_path)
+ensembles <- do.call(rbind,lapply(file_names, read.csv))
+
+ensembles = subset(ensembles, dataset != "fertility_yesnoreal")
+ensembles = subset(ensembles, dataset != "fertility_yesnoOHE")
+#ensembles = subset(ensembles, model != "MockClassifier")
+
+ensembles$ensemble_all = paste(ensembles$model, ensembles$ensemble, sep="_")
+ensembles$ensemble_all = paste(ensembles$ensemble_all, ensembles$voting_system, sep="_")
+
+ggplot(ensembles, aes(ensemble_size, score, colour=ensemble_all)) + 
+  geom_point() + geom_line() + facet_wrap(~dataset) # + geom_smooth(aes(group=ensemble_all), se = FALSE)
+
+png_name <- paste("full_results", ".png", sep="")
+png_path <- file.path(script.dir, "..", "png", png_name)
+ggsave(png_path, width = 120, height = 60, units = "cm")
